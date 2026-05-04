d9ecff7b5f2833a24510bba8237e1e4013408104
U-SKU2\Sushko <Sushko@ARM-246-10.SKU2.Agat.by>
2026-05-04 13:42:43 +0300

    neovim config

---
+++ .config/nvim/lua/config/settings.lua
@@ -0,0 +1,22 @@
+vim.cmd("language en_US")
+
+--vim.opt.clipboard = 'unnamedplus' -- clipboard
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+vim.opt.mouse = 'a'
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.termguicolors = true
+
+vim.opt.incsearch = true
+vim.opt.hlsearch = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
